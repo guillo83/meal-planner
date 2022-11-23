d61c56b475535aae7ab93d655b37ca3e4e839a79
Guillermo Ortiz Pena <g.ortizp@gmail.com>
2022-11-23 19:12:06 +0000

    Add http routes

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-from mylib.logic import wiki
+from fastapi import FastAPI
+import uvicorn
+from mylib.logic import wiki as get_wiki
+from mylib.logic import search_wiki
 
-print(wiki())
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Wikipedia API. Call /search or /wiki"}
+
+@app.get("/search/{value}")
+async def search(value: str):
+    """Page to search in wikipedia"""
+    result = search_wiki(value)
+    return {"result": result}
+
+@app.get("/wiki/{value}")
+async def wiki_logic(value: str):
+    """Retrieve wikipedia page"""
+    result =get_wiki(value)
+    return {"result": result}
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='0.0.0.0')
