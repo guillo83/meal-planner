6405b923cdd3876c8ade156afc3ed835c427288b
Guillermo Ortiz <guillermo.ortiz@Guillermos-MBP.home>
2022-11-29 12:38:57 -0800

    Update meal planner java

---
+++ src/java/planner/src/main/java/com/gdev/planner/mealplanner/MealPlannerApplication.java
@@ -0,0 +1,21 @@
+package com.gdev.planner.mealplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MealPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealPlannerApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
